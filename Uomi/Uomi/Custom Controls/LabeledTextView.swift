a9d1dd462aba2fcd567b6f6ccf6c3f0adf4bf454
Eric Gonzalez <erigonz89@gmail.com>
2017-11-09 17:52:36 -0600

    Introduced custom labeledtextview class

---
+++ Uomi/Uomi/Custom Controls/LabeledTextView.swift
@@ -0,0 +1,64 @@
+//
+//  LabeledTextView.swift
+//  Uomi
+//
+//  Created by Eric Gonzalez on 11/9/17.
+//  Copyright © 2017 Eric Gonzalez. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable class LabeledTextView: UIView {
+
+    var view: UIView!
+    
+    override init(frame: CGRect) {
+        // 1. setup any properties here
+        
+        // 2. call super.init(frame:)
+        super.init(frame: frame)
+        
+        // 3. Setup view from .xib file
+        xibSetup()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        // 1. setup any properties here
+        
+        // 2. call super.init(coder:)
+        super.init(coder: aDecoder)
+        
+        // 3. Setup view from .xib file
+        xibSetup()
+    }
+    
+    func xibSetup() {
+        view = loadViewFromNib()
+        
+        // use bounds not frame or it'll be offset
+        view.frame = bounds
+        
+        // Make the view stretch with containing view
+        view.autoresizingMask = [UIViewAutoresizing.flexibleWidth, UIViewAutoresizing.flexibleHeight]
+        // Adding custom subview on top of our view (over any custom drawing > see note below)
+        addSubview(view)
+    }
+    
+    func loadViewFromNib() -> UIView {
+
+        let bundle = Bundle(for: type(of: self))
+        let nib = UINib(nibName: "LabeledTextView", bundle: bundle)
+        let view = nib.instantiate(withOwner: self, options: nil)[0] as! UIView
+        
+        return view
+    }
+    
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
