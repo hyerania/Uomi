06119db2077811fc5c9928fba3f3cf2b27c5fea8
Kevin J Nguyen <kevinjnguyen2@gmail.com>
2017-11-14 15:37:46 -0600

    Implemented load id for AccountManager

--- Uomi/Uomi/AccountManagement/AccountManager.swift
+++ Uomi/Uomi/AccountManagement/AccountManager.swift
@@ -7,18 +7,48 @@
 //
 
 import Foundation
+import FirebaseDatabase
+//protocol AccountManager {
+//    func register(user: String, name: String, password: String) -> (User, Bool)
+//
+//    func login(email: String, password: String)
+//
+//    // clear cache
+//    func logout()
+//
+//    func userExists(email: String) -> Bool
+//
+//    func load(id: String) -> User
+//
+//    func users(forEvent: Event) -> [User]
+//}
 
-protocol AccountManager {
-    func register(user: String, name: String, password: String) -> (User, Bool)
+class AccountManager {
+    var ref: DatabaseReference!
     
-    func login(email: String, password: String)
+    func load(id: String, completionHandler: @escaping(User?) -> ()) {
+        ref.child("accounts").child(id).observeSingleEvent(of: .value, with: { (snapshot) in
+            guard
+                let userData = snapshot.value as? NSDictionary,
+                let email = userData["email"] as? String,
+                let name = userData["name"] as? String
+            else {
+                print("Unable to grab user information")
+                completionHandler(nil)
+                return
+            }
+            
+            let user = User(uid: id, email: email, name: name)
+            completionHandler(user)
+            
+        })
+    }
     
-    // clear cache
-    func logout()
+    private init() {
+        print("Account mananger constructor called...")
+        ref = Database.database().reference()
+    }
     
-    func userExists(email: String) -> Bool
+    static let sharedInstance = AccountManager()
     
-    func load(id: String) -> User
-    
-    func users(forEvent: Event) -> [User]
 }
